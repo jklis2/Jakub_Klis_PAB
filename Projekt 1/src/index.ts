2876df55f82fe56ab00f5c895a05be71038e31fa
Jakub Kliś <jakubklis2201@gmail.com>
2022-03-16 17:03:08 +0100

    Koniec

--- Projekt 1/src/index.ts
+++ Projekt 1/src/index.ts
@@ -1,10 +1,36 @@
 const express = require('express')  
 const app = express()  
 app.get('/', function (req, res) {  
-  const num1 = +req.query.num1
-  const num2 = +req.query.num2
-  const operation = req.query.operation;
+  res.send("Calculatorrr") 
 })  
 app.listen(3000)  
+function calculate(operation: string, num1: number, num2: number) {
+  if(operation == 'dodawanie')
+  {
+      return (num1+'+'+num2+'='+(num1+num2))
+  }
+  else if(operation == 'odejmowanie')
+  {
+      return (num1+'-'+num2+'='+(num1-num2))
+  }
+  else if(operation == 'mnozenie')
+  {
+      return (num1+'*'+num2+'='+(num1*num2))
+  }
+  else if(operation == 'dzielenie')
+  {
+      return (num1+'/'+num2+'='+(num1/num2))
+  }
+  else
+  {
+      return ('Niepoprawna komenda! Mozesz tylko dodawac, odejmowac, mnozyc i dzielic.')
+  }
+}
+app.get('/:operation/:num1/:num2', function(req, res) {
+  let operation = req.params.operation
+  let num1 = parseInt(req.params.num1)
+  let num2 = parseInt(req.params.num2)
 
-// localhost:3000?num1=4&num2=5&op=add
+  res.send(calculate(operation, num1, num2))
+
+}) 
